6dc1a45b9111a59db9d48acd21ab59e8cfe065d9
Susielove <44665170+Susielove@users.noreply.github.com>
2018-12-14 16:04:46 +0800

    cookies不放headers里面

---
+++ 08-renren_login.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://www.renren.com/PLogin.do"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"}
+cookies= "anonymid=jpf70le8-9ae8tv; depovince=JS; jebecookies=44141ed3-133c-4e43-bee6-c06ad192477d|||||; _r01_=1; JSESSIONID=abcl3nDsnXPdQ7k7R6mEw; ick_login=4c1aad9e-79e6-4959-90cd-02b3011f795a; _de=4CF6CB302AA2195C2C7FD618744BFAA5; p=1c1aae36295ac84d281a8cb74f27a9e50; first_login_flag=1; ln_uact=13862332046; ln_hurl=http://head.xiaonei.com/photos/0/0/men_main.gif; t=e91b5da895f7429daba967856be7a78e0; societyguester=e91b5da895f7429daba967856be7a78e0; id=969029780; xnsid=7312785c; ver=7.0; loginfrom=null; jebe_key=09519390-cd6a-4a60-9423-0e0cd42bc289%7C035f4dae732f625f4302ef7a8a179598%7C1544257637927%7C1%7C1544257636376; wp_fold=0"
+cookies = {i.split("=")[0]:i.split("=")[1] for i in cookies.split(";")} #先将cookies切割，得到一个列表，格式为【"....=....","...=...."】,再对里面切割
+print(cookies)
+response = requests.get(url, headers=headers, cookies=cookies)
+
+with open("renren.html", "w", encoding="utf-8") as f:
+    f.write(response.content.decode())
